a84e044555befc12db12a8ea869ddf4fd8b79309
kanlar75 <kanlar75@gmail.com>
2023-07-04 18:54:12 +0300

    рабочий вариант

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,12 @@
 import json
+import requests
 import os
 import fake_useragent
 from abc import ABC, abstractmethod
 from pprint import pprint
 
-import requests
-
-from src.json_saver import OperatingJSON
-
-pages = 1
-vacancies_on_page = 20
+pages = 5
+vacancies_on_page = 100
 
 
 class FindVacancies(ABC):
@@ -22,138 +19,99 @@ class FindVacancies(ABC):
 class FileHandling(ABC):
 
     @abstractmethod
-    def add_vacancies(self, vacancy):
+    def save_vacancies(self, list_dict):
         pass
 
     @abstractmethod
-    def get_vacancies(self, vacancy):
+    def get_vacancies_by_salary(self, list_dict):
         pass
 
     @abstractmethod
-    def delete_vacancy(self, vacancy):
+    def delete_vacancy(self, vacancy_str):
         pass
-
-
-def instantiate_from_data_hh():
-    """ Инициализирует экземпляры класса из данных HeadHunterAPI """
-    with open('tempHH.json', 'r', encoding='utf8', ) as file:
-
-        data_all = json.load(file)
-
-        for data_hh in data_all:
-
-            for j in range(len(data_hh['items'])):
-                title = data_hh['items'][j]['name']
-                url = data_hh['items'][j]['alternate_url']
-                if data_hh['items'][j]['salary'] is not None:
-                    salary_from = data_hh['items'][j]['salary']['from']
-                    salary_to = data_hh['items'][j]['salary']['to']
-                    currency = data_hh['items'][j]['salary'][
-                        'currency']
-                else:
-                    salary_from = 0
-                    salary_to = 0
-                    currency = 'не указано'
-                description = data_hh['items'][j]['snippet'][
-                    'responsibility']
-                town = data_hh['items'][j]['area']['name']
-                education = data_hh['items'][j]['snippet']['requirement']
-                experience = data_hh['items'][j]['experience']['name']
-                Vacancy(title=title, url=url,
-                        salary_from=salary_from,
-                        salary_to=salary_to,
-                        currency=currency,
-                        description=description,
-                        town=town,
-                        education=education,
-                        experience=experience
-                        )
 
 
 class HeadHunterAPI(FindVacancies):
     headers = {'user-agent': fake_useragent.UserAgent().random}
-    params = {'per_page': vacancies_on_page}
-    data = {}
-    vac = []
+    params = {'per_page': vacancies_on_page, 'archive': False}
 
     def __init__(self):
+        # self.data = data
         pass
 
     def get_vacancies(self, vacancy):
         self.params['text'] = vacancy
+        vac_list_hh = []
 
-        for i in range(pages):
-            self.params['page'] = i
-            self.data = (requests.get('https://api.hh.ru/vacancies',
-                                      params=self.params,
-                                      headers=self.headers)).json()
-            self.vac.append(self.data)
-        self.save_vacancies_temp()
-        return self.vac
+        for page in range(pages):
+            data_hh = requests.get('https://api.hh.ru/vacancies',
+                                   params=self.params,
+                                   headers=self.headers).json()  # Запрос к API
 
-    def save_vacancies_temp(self):
-        with open('tempHH.json', 'w', encoding='utf-8', ) as file:
-            json.dump(self.vac, file, indent=2, ensure_ascii=False)
-
-
-def instantiate_from_data_sj():
-    """ Инициализирует экземпляры класса из данных SuperJobAPI """
-
-    with open('tempSJ.json', 'r', encoding='utf8', ) as file:
-
-        data_all = json.load(file)
-
-        for data_sj in data_all:
-            for j in range(len(data_sj['objects'])):
-                title = data_sj['objects'][j]['profession']
-                url = data_sj['objects'][j]['link']
-                salary_from = data_sj['objects'][j]['payment_from']
-                salary_to = data_sj['objects'][j]['payment_to']
-                currency = data_sj['objects'][j]['currency']
-                description = data_sj['objects'][j]['candidat']
-                town = data_sj['objects'][j]['town']['title']
-                education = data_sj['objects'][j]['education']['title']
-                experience = data_sj['objects'][j]['experience']['title']
-                Vacancy(title=title, url=url,
-                        salary_from=salary_from,
-                        salary_to=salary_to,
-                        currency=currency,
-                        description=description,
-                        town=town,
-                        education=education,
-                        experience=experience
-                        )
+            for i in range(vacancies_on_page):
+                vac_dict_hh = {'title': data_hh['items'][i]['name'],
+                               'url': data_hh['items'][i]['alternate_url']}
+                if data_hh['items'][i]['salary'] is not None:
+                    vac_dict_hh['salary_from'] = data_hh['items'][i]['salary'][
+                        'from']
+                    vac_dict_hh['salary_to'] = data_hh['items'][i]['salary'][
+                        'to']
+                    vac_dict_hh['currency'] = data_hh['items'][i]['salary'][
+                        'currency']
+                else:
+                    vac_dict_hh['salary_from'] = 0
+                    vac_dict_hh['salary_to'] = 0
+                    vac_dict_hh['currency'] = 0
+                vac_dict_hh['description'] = data_hh['items'][i]['snippet'][
+                    'responsibility']
+                vac_dict_hh['town'] = data_hh['items'][i]['area']['name']
+                vac_dict_hh['education'] = data_hh['items'][i]['snippet'][
+                    'requirement']
+                vac_dict_hh['experience'] = data_hh['items'][i]['experience'][
+                    'name']
+                vac_list_hh.append(vac_dict_hh)
+        return vac_list_hh
 
 
 class SuperJobAPI(FindVacancies):
-    # API_TOKEN_SJ = os.getenv('API_TOKEN_SJ')
     API_TOKEN_SJ = 'v3.r.13773054.322adb5f53925517e4e5d74ec1b4ac2fc2fb5d21' \
                    '.643a11cd83b584cd8028edf34a0e6ef951dd8e1e'
     headers = {"X-Api-App-Id": API_TOKEN_SJ}
     params = {"count": vacancies_on_page}
-    data = {}
 
     def __init__(self):
         self.vac = []
 
     def get_vacancies(self, vacancy):
         self.params['keyword'] = vacancy
-        for i in range(pages):
-            self.params['page'] = i
-            self.data = (requests.get("https://api.superjob.ru/2.0/vacancies/",
-                                      params=self.params,
-                                      headers=self.headers)).json()
-            self.vac.append(self.data)
-        self.save_vacancies_temp()
-        return self.vac
+        vac_list_sj = []
+        for page in range(pages):
+            data_sj = (requests.get("https://api.superjob.ru/2.0/vacancies/",
+                                    params=self.params,
+                                    headers=self.headers)).json()  # Запрос к API
 
-    def save_vacancies_temp(self):
-        with open('tempSJ.json', 'w', encoding='utf-8', ) as file:
-            json.dump(self.vac, file, indent=2, ensure_ascii=False)
+            for i in range(vacancies_on_page):
+                vac_dict_sj = {'title': data_sj['objects'][i]['profession'],
+                               'url': data_sj['objects'][i]['link'],
+                               'salary_from': data_sj['objects'][i][
+                                   'payment_from'],
+                               'salary_to': data_sj['objects'][i][
+                                   'payment_to'],
+                               'currency': data_sj['objects'][i]['currency'],
+                               'description': data_sj['objects'][i][
+                                   'candidat'],
+                               'town': data_sj['objects'][i]['town']['title'],
+                               'education': data_sj['objects'][i][
+                                   'education']['title'],
+                               'experience': data_sj['objects'][i][
+                                   'experience']['title']}
+                vac_list_sj.append(vac_dict_sj)
+            return vac_list_sj
 
 
 class Vacancy:
-    vacancy_list = []
+    vac_list = []
+    data_list = []
 
     def __init__(self, title, url, salary_from, salary_to, currency,
                  description, town, education, experience):
@@ -174,18 +132,43 @@ class Vacancy:
         self.url = url if url else 'не указано'
         self.salary_from = salary_from if salary_from else 0
         self.salary_to = salary_to if salary_to else 0
-        self.currency = currency if currency else 'не указано'
+        self.currency = 'RUR' if currency in ['RUR', 'rub'] else currency
         self.description = description if description else 'не указано'
         self.town = town if town else 'не указано'
         self.education = education if education else 'не указано'
         self.experience = experience if experience else 'не указано'
-        self.vacancy_list.append(self)
+        self.vac_list.append(self)
 
-    # def __repr__(self):
-    #     return (f"Vacancy(name={self.title}, salary_from={self.salary_from}, "
-    #             f"salary_to={self.salary_to}, "
-    #             f"salary_currency={self.currency}, town={self.town},"
-    #             f"experience={self.experience}, url={self.url}")
+    @classmethod
+    def instantiate_from_data(cls, instantiate_list):
+        """ Инициализирует экземпляры класса Vacancy из отформатированного
+        списка """
+
+        cls.data_list = instantiate_list
+        for data in cls.data_list:
+            title = data['title']
+            url = data['url']
+            salary_from = data['salary_from'] if ['salary_from'] else 0
+            salary_to = data['salary_to'] if ['salary_to'] else 0
+            currency = data['currency'] if ['currency'] else 0
+            description = data['description']
+            town = data['town']
+            education = data['education']
+            experience = data['experience']
+            Vacancy(title=title, url=url,
+                    salary_from=salary_from,
+                    salary_to=salary_to,
+                    currency=currency,
+                    description=description,
+                    town=town,
+                    education=education,
+                    experience=experience
+                    )
+
+    # def __repr__(self): return (f"Vacancy(name={self.title}, salary_from={
+    # self.salary_from}, " f"salary_to={self.salary_to},
+    # " f"salary_currency={self.currency}, town={self.town}," f"experience={
+    # self.experience}, url={self.url}")
 
     def __str__(self):
         return f'Вакансия: {self.title}\nСсылка на вакансию: {self.url}\n' \
@@ -196,17 +179,19 @@ class Vacancy:
                f'Зарплата до: ' \
                f'{self.salary_to if self.salary_to != 0 else "не указано"}' \
                f' {self.currency if self.salary_to != 0 else ""}\n' \
-               f'Описание: {self.description}\nГород: {self.town}\n' \
-               f'Требования: {self.education}\nОпыт: {self.experience}\n'
+               f'Описание: {self.description}' \
+               f'\nГород: {self.town}\n' \
+               f'Образование: {self.education}\n' \
+               f'Опыт: {self.experience}\n'
 
     def __eq__(self, other):
-        """ Возвращает True или False (равенство зарплаты) """
+        """ Возвращает True или False (равенство минимальной зарплаты) """
 
         if self.validate(other):
             return self.salary_from == other.salary_from
 
     def __gt__(self, other):
-        """ Возвращает True или False, сравнение по зарплате """
+        """ Возвращает True или False, сравнение по минимальной зарплате """
 
         if self.validate(other):
             return self.salary_from > other.salary_from
@@ -217,73 +202,121 @@ class Vacancy:
         if self.validate(other):
             return self.salary_from >= other.salary_from
 
-    @classmethod
-    def validate(cls, obj):
-        """ Проверяет что зарплата int. """
-        if not isinstance(obj.salary_from, int):
-            return False
-        else:
+    def validate(self, obj):
+        """ Проверяет что зарплата в рублях """
+
+        if self.currency and obj.currency in ["rub", "RUR"]:
             return True
-
-    @classmethod
-    def vacancies_sorted(cls):
-        cls.vacancy_list.sort(key=lambda lst_: lst_.salary_from, reverse=True)
-        return cls.vacancy_list
-
-    def save(self):
-        with open('temp.json', 'w', encoding='utf8') as file:
-            json.dump(self.vacancy_list.__dict__, file, indent=2,
-                      ensure_ascii=False)
+        else:
+            return False
 
 
 class JSONSaver(FileHandling):
 
-    def __init__(self):
-        self.data_lst = None
+    def __init__(self, file_name='data.json'):
+        self.file_name = file_name
 
-    def add_vacancies(self, vacancy):
-        with open('vacancies.json', 'a', encoding='utf8') as file:
-            json.dump(vacancy.__dict__, file, indent=2, ensure_ascii=False)
+    @staticmethod
+    def json_exemplars(vac_obj_list) -> list[dict]:
+        """
+        Метод для конвертации списка вакансий из списка экземпляров класса
+        в список словарей dict
 
-    def get_vacancies(self, vacancy_salary):
-        with open('temp.json', 'r', encoding='utf8') as f:
-            self.data_lst = json.load(f)
-            for vacancy in self.data_lst:
-                if vacancy.salary_from >= vacancy_salary:
-                    return vacancy
+        """
+        list_dicts = []
+        for obj in vac_obj_list:
+            list_dicts.append(obj.__dict__)
+        return list_dicts
+
+    @staticmethod
+    def save_vacancies(list_dict, file_name='data.json'):
+        """ Метод для загрузки списка словарей в JSON файл """
+
+        with open(file_name, "w+", encoding="utf-8") as file:
+            json.dump(list_dict, file, indent=2, ensure_ascii=False)
+
+    def get_vacancies_town(self, find_str):
+        Vacancy.vac_list = []
+        find_list = []
+        with open(self.file_name, 'r', encoding='utf8') as file:
+            data_lst = json.load(file)
+            for data in data_lst:
+                if find_str in data.values():
+                    find_list.append(data)
                 else:
-                    return f'Подходящих вакансий не найдено'
+                    continue
 
-    def delete_vacancy(self, vacancy):
-        pass
+        return find_list
+
+    def get_vacancies_salary_max(self):
+        Vacancy.vac_list = []
+        find_list = []
+        with open(self.file_name, 'r', encoding='utf8') as file:
+            data_lst = json.load(file)
+            for data in data_lst:
+                if data['currency'] == 'RUR':
+                    find_list.append(data)
+                else:
+                    continue
+            find_list.sort(key=lambda lst_: lst_['salary_from'], reverse=True)
+        return find_list
+
+    def get_vacancies_by_word(self, *args):
+        Vacancy.vac_list = []
+        find_list = []
+        with open(self.file_name, 'r', encoding='utf8') as file:
+            data_lst = json.load(file)
+            for data in data_lst:
+                for arg in args:
+                    if arg in data['description']:
+                        find_list.append(data)
+                    else:
+                        continue
+            find_list.sort(key=lambda lst_: lst_['salary_from'], reverse=True)
+        return find_list
+
+    def get_vacancies_by_salary(self, salary):
+        """
+        Метод для фильтрации вакансий из файла JSON по размеру зарплаты
+        (минимальной)
+        """
+        Vacancy.vac_list = []
+        find_list = []
+        with open(self.file_name, "r", encoding="utf-8") as file:
+            data_lst = json.load(file)
+            find_list = []
+            for data in data_lst:
+                if data["salary_from"] <= int(salary) or data['currency'] \
+                        != 'RUR':
+                    continue
+                else:
+                    find_list.append(data)
+            find_list.sort(key=lambda lst_: lst_['salary_from'], reverse=True)
+        return find_list
+
+    def delete_vacancy(self, salary):
+        find_list = []
+        with open(self.file_name, 'r', encoding='utf8') as file:
+            data_lst = json.load(file)
+            for data in data_lst:
+                if data['salary_from'] <= int(salary):
+                    continue
+                else:
+                    find_list.append(data)
+            find_list.sort(key=lambda lst_: lst_['salary_from'], reverse=True)
+        return find_list
 
 
-vacancies_sj = SuperJobAPI()
-vacancies_hh = HeadHunterAPI()
 
-vacancies_sj.get_vacancies('python')
-vacancies_hh.get_vacancies('python')
-instantiate_from_data_sj()
-instantiate_from_data_hh()
-
-vacancy1 = Vacancy.vacancy_list[0]
-print(vacancy1)
-#
-vacancy2 = Vacancy.vacancy_list[1]
-
-# print(vacancy2)
-# print(len(Vacancy.vacancy_list))
-# for i in range(500):
-#     print(i, Vacancy.vacancy_list[i].salary_from)
-# json_saver = JSONSaver()
-# json_saver.add_vacancy(vacancy1)
-# json_saver.add_vacancy(vacancy2)
-# with open('tempHH.json', 'r', encoding='utf8', ) as file:
-#     data = json.load(file)[0]
-#     for i in range(100):
-#         pprint(data['items'][i]['name'])
-# vacancies_list = Vacancy.vacancy_list
-# vacancies_list.vacancies_sorted()
-# print(vacancies_list[1])
-file = OperatingJSON()
-file.json_exemplars(Vacancy.vacancy_list)
+# vacancies_sj = SuperJobAPI()
+# vacancies_hh = HeadHunterAPI()
+# list_sj = vacancies_sj.get_vacancies('python')
+# list_hh = vacancies_hh.get_vacancies('python')
+# Vacancy.instantiate_from_data(list_hh)
+# Vacancy.instantiate_from_data(list_sj)
+# f = JSONSaver()
+# f.save_vacancies(f.json_exemplars(Vacancy.vac_list))
+# Vacancy.instantiate_from_data(f.delete_vacancy(45000))
+# f.save_vacancies(f.json_exemplars(Vacancy.vac_list))
+# for v in Vacancy.vac_list:
+#     print(v)
